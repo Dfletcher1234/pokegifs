05a2cbf9a282f759841b8bb0cc1c143492ebd7ef
David FletcherE <dtsfletcher@gmail.com>
2017-09-26 16:58:08 -0400

    DONE

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,20 +1,26 @@
 class PokemonController < ApplicationController
 
-def index
-@pokeman = Pokemon.all
-end
+
 
 
 def show
   res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
   body = JSON.parse(res.body)
 
-  name  = body["name"]
+  res1 = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=OfdmRoqCqDyM1zt0L8XZUl2QciOssj9o&q=pikachu&rating=g")
+  body1 = JSON.parse(res1.body)
 
+# byebug
+render json: {
 
-  render json: {
-        id: result["id"]
-        name: result['name'],
-        image: result['image_thumb_url']
-      }
+    id: body["id"],
+    name: body["name"],
+    # types: body["types"].find { |type| type[:type] },
+    types: body["types"].map { |x| x["type"]["name"] },
+
+    gif_url: body1["data"][0]["url"]
+
+  }
+
+    end
 end
